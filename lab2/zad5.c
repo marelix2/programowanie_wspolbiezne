c796ac5cf9b50600377324f59a9a836558d5dd80
marelix2 <marelix2@gmail.com>
2017-10-22 23:14:55 +0200

    zad5

---
+++ lab2/zad5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(){
+	int cpid,
+	int	status, 
+	int i; 
+	
+	for(i = 0; i < 5; i++){
+		if ((cpid = fork()) == -1) exit(1);
+		else if (cpid == 0){
+			printf("Proces potomny o pidzie %d z rodzica %d\n",getpid(),getppid());
+			sleep(2);
+			exit(1);
+		} else {
+			printf("Proces macierzysty o pidzie %d i dziecku %d\n",getpid(),cpid);
+			wait();
+		}
+	}
+
+	exit(0);    
+}

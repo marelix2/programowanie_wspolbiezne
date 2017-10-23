ae9ae3d811247efab24fc80a947c6503778c6b2a
marelix2 <marelix2@gmail.com>
2017-10-23 22:25:42 +0200

    poprawki

--- lab2/zad5.c
+++ lab2/zad5.c
@@ -1,24 +1,18 @@
+
 #include <stdio.h>
 #include <stdlib.h>
-
-
-
 int main(){
-	int cpid;
-	int	status; 
-	int i; 
-	
-	for(i = 0; i < 5; i++){
-		if ((cpid = fork()) == -1) exit(1);
-		else if (cpid == 0){
-			printf("Proces potomny o pidzie %d z rodzica %d\n",getpid(),getppid());
-			sleep(2);
-			exit(1);
-		} else {
-			printf("Proces macierzysty o pidzie %d i dziecku %d\n",getpid(),cpid);
-			wait();
-		}
-	}
-
-	exit(0);    
+int cpid;
+int    status;
+int i,j;
+for(i = 0; i < 5; i++){
+if ((cpid = fork()) == -1) exit(1);
+else if (cpid == 0){
+printf("Proces potomny o pidzie %d z rodzica %d\n",getpid(),getppid());
+sleep(5);
+exit(2);
+}
+}
+for(j =0; j<5; j++) wait(NULL);
+exit(0);
 }
--- lab2/zad6.c
+++ lab2/zad6.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int main(){
-	int cpid;
-	int	status; 
-	int i; 
-	
-	for(i = 0; i < 5; i++){
-		if ((cpid = fork()) == -1) exit(1);
-		else if (cpid == 0){
-			printf("Proces potomny o pidzie %d z rodzica %d\n",getpid(),getppid());
-			 
-			 if(i != 4) wait(1);
-			 else sleep(5);
-		} else {
-			printf("Proces macierzysty o pidzie %d i dziecku %d\n",getpid(),cpid);
-			wait();
-		}
-	}
+int cpid;
+int    status;
+int i;
+for(i = 0; i < 5; i++){
+if ((cpid = fork()) == -1) exit(1);
 
-	exit(0);    
+if (cpid == 0){
+printf("Proces potomny o pidzie %d z rodzica %d\n",getpid(),getppid());
+if(i != 4) wait(1);
+else sleep(5);
+}
+else {
+wait(NULL);
+exit(1);
+
+}
+}
+exit(0);
 }

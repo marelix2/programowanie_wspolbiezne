70eb9f940f601ac8405cf505d4fd9859bce11daf
marelix2 <marelix2@gmail.com>
2017-10-29 19:17:28 +0100

    lab3

--- lab3/zad.c
+++ lab3/zad.c
@@ -1,63 +1,74 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+    #include <stdlib.h>
+    #include <unistd.h>
+    #include <string.h>
+    #include <sys/types.h>
+    #include <sys/wait.h>
+     
+     
+    int main(){
  
-int n;
-char buff[100], buff2[100], filename[100];
-int pipechild[2], pipeparent[2];
-int childpid;
+    int lengthOfString;
+    char buffor1[100];
+    char buffor2[100];
+    char filename[100];
+    int childPipe[2];
+    int parentPipe[2];
+    int pid;
  
-int main(){
-    if(pipe(pipechild) < 0)
-        perror("Blad otwierania pipe_c\n");
-    if(pipe(pipeparent) < 0)
-        perror("Blad otwierania pipe_s\n");
+    const int R = 1;
+    const int W = 0;
  
-    if((childpid = fork()) == -1){
-        perror("Blad fork\n");
-    } else {
-        if(childpid == 0){
-            close(pipechild[1]);
-            close(pipeparent[0]);
  
-            if((n = read(pipechild[0], buff, sizeof(buff))) <= 0)
-                perror("Blad odczytu pipechild[0]\n");
  
-            FILE *file = fopen(buff, "r");
- 
-            if(file == NULL){
-                if(write(pipeparent[1], "Blad otwarcia pliku\n", 20) != 20)
-                    perror("Blad zapisu pipserver[1]\n");
-            } else {
-                while(fgets(buff, sizeof(buff), file) != NULL)
-                            write(pipeparent[1], buff, sizeof(buff));
-            }
- 
-            fclose(file);
- 
-            close(pipechild[0]);
-            close(pipeparent[1]);
+        if(pipe(childPipe) < 0)
+            perror("Blad otwierania pipe_c\n");
+        if(pipe(parentPipe) < 0)
+            perror("Blad otwierania pipe_s\n");
+     
+        if((pid = fork()) == -1){
+            perror("Nie utworzono potomka\n");
         } else {
-            close(pipechild[0]);
-            close(pipeparent[1]);
- 
-            printf("Podaj nazwe pliku: ");     
-            scanf("%s", filename);
- 
-            if(write(pipechild[1], filename, sizeof(filename)) != sizeof(filename))
-                perror("Blad zapisu pipechild[1]\n");
- 
-            while(read(pipeparent[0], buff2, sizeof(buff2)) > 0)
-                            write(1, buff2, strlen(buff2));
- 
-            wait(NULL);
-            close(pipechild[1]);
-            close(pipeparent[0]);
+            if(pid == 0){
+                close(childPipe[R]);
+                close(parentPipe[W]);
+     
+                if((lengthOfString = read(childPipe[W], buffor1, sizeof(buffor1))) <= 0)
+                    perror("Blad odczytu \n");
+     
+                FILE *file = fopen(buffor1, "r");
+     
+                if(file == NULL){
+            char blad[20] = "Blad otwarcia pliku\n";
+                    if(write(parentPipe[R], blad, sizeof(blad)) != 20)
+                        perror("Blad zapisu \n");
+                } else {
+                    while(fgets(buffor1, sizeof(buffor1), file) != NULL)
+                                write(parentPipe[R], buffor1, sizeof(buffor1));
+                }
+     
+                fclose(file);
+     
+                close(childPipe[W]);
+                close(parentPipe[R]);
+            } else {
+                close(childPipe[W]);
+                close(parentPipe[R]);
+     
+                printf("Podaj nazwe pliku: ");    
+                scanf("%s", filename);
+     
+                if(write(childPipe[R], filename, sizeof(filename)) != sizeof(filename))
+                    perror("Blad zapisu \n");
+     
+                while(read(parentPipe[W], buffor2, sizeof(buffor2)) > 0)
+                                write(R, buffor2, strlen(buffor2));
+     
+                wait(NULL);
+                close(childPipe[R]);
+                close(parentPipe[W]);
+            }
         }
+     
+        return 0;
     }
- 
-    return 0;
-}
